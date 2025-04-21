14535211cff04985772780c1da83dc88d0929273
Dragan Petrovic <pdragan1996@gmail.com>
2025-04-21 14:08:44 +0200

    Adding trends skeleton

---
+++ src/components/trends/components/trending-list-skeleton.tsx
@@ -0,0 +1,46 @@
+export const TrendingListSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+      <div className="rounded-xl shadow border p-4 flex flex-col gap-1">
+        <div className="bg-gray-200 rounded w-1/2 h-[28px]" />
+        <div className="bg-gray-200 rounded w-3/4 h-[38px]" />
+      </div>
+    </div>
+  );
+};
