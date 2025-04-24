5a42bb0fff30eeafa73e4b4d2eabdb5640dbee6c
Dragan Petrovic <pdragan1996@gmail.com>
2025-04-24 17:06:25 +0200

    Added console.log for logging scrapers

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,7 @@ export function middleware(request: NextRequest) {
   const isSuspiciousBot = /curl|node-fetch|python|axios|scrapy|go-http-client/i.test(ua);
 
   if (isSuspiciousBot) {
+    console.log("Scraper found !!!!");
     return new NextResponse("Blocked scraper", { status: 403 });
   }
 
